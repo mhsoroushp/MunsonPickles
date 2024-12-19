2823c09a5d11d12f2591ef2fcea1883c067891de
mhSoroush <mohammadHadi.soroush@stud.unibas.ch>
2024-12-19 14:29:56 +0100

    move Dockerfile one folder up

--- MunsonPickles.Web/obj/Debug/net7.0/MunsonPickles.Web.AssemblyInfo.cs
+++ MunsonPickles.Web/obj/Debug/net7.0/MunsonPickles.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MunsonPickles.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654e5abc20f212d36bf7791155e6b04def132c1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MunsonPickles.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MunsonPickles.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
